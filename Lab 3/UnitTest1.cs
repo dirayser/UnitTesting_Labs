69104ad4cd3bf9b5d759817481d0c85db9fd713a
Dmitriy Dmitriyev <45948880+dirayser@users.noreply.github.com>
2021-06-05 20:03:12 +0300

    Update UnitTest1.cs

--- Lab 3/UnitTest1.cs
+++ Lab 3/UnitTest1.cs
@@ -3,6 +3,7 @@ using Xunit;
 
 namespace IIG.PasswordHashingUtils
 {
+    [Collection("Sequential")]
     public class Test_Equals_ExpectTrue
     {
         [Fact]
@@ -42,6 +43,7 @@ namespace IIG.PasswordHashingUtils
         }
     }
 
+    [Collection("Sequential")]
     public class Test_Equals_ExpectFalse
     {
         [Fact]
@@ -64,11 +66,215 @@ namespace IIG.PasswordHashingUtils
 
         [Fact]
         public void Test_sameSaltwithSpace_samePasswords()
-        {
+        { 
             PasswordHasher.Init(null, 1);
             var hash = PasswordHasher.GetHash("12345", " salt");
             var hash2 = PasswordHasher.GetHash("12345", "salt");
             Assert.NotEqual(hash, hash2);
         }
+    }
+
+    [Collection("Sequential")]
+    public class Main_Tests
+    {
+        [Fact]
+        public void Route_0_1()
+        {
+            var hash = PasswordHasher.GetHash("password", "");
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4()
+        {
+            var hash = PasswordHasher.GetHash("password", "salt");
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash("password", dp);
+            Assert.NotNull(hash);
+        }
+        [Fact]
+        public void Route_0_1_5()
+        {
+            var hash = PasswordHasher.GetHash("password", "", 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_1_6()
+        {
+            var hash = PasswordHasher.GetHash("password", "", 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4_5()
+        {
+            var hash = PasswordHasher.GetHash("password", "salt", 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4_5()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash("password", dp, 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4_6()
+        {
+            var hash = PasswordHasher.GetHash("password", "salt", 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4_6()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash("password", dp, 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_1_5_10_11()
+        {
+            var hash = PasswordHasher.GetHash(null, "", 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_1_6_10_11()
+        {
+            var hash = PasswordHasher.GetHash(null, "", 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4_5_10_11()
+        {
+            var hash = PasswordHasher.GetHash(null, "salt", 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4_5_10_11()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash(null, dp, 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4_6_10_11()
+        {
+            var hash = PasswordHasher.GetHash(null, "salt", 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4_6_10_11()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash(null, dp, 9310);
+            Assert.NotNull(hash);
+        }
+
+  
+        [Fact]
+        public void Route_0_1_5_7_8_9_11()
+        {
+            var hash = PasswordHasher.GetHash(null, "", 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_1_6_7_8_9_11()
+        {
+            var hash = PasswordHasher.GetHash("password", "", 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4_5_7_8_9_11()
+        {
+            var hash = PasswordHasher.GetHash("password", "salt", 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4_5_7_8_9_11()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash("password", dp, 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4_6_7_8_9_11()
+        {
+            var hash = PasswordHasher.GetHash("password", "salt", 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4_6_7_8_9_11()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash("password", dp, 9310);
+            Assert.NotNull(hash);
+        }
+
+
+        [Fact]
+        public void Route_0_1_5_7_9_11()
+        {
+            var hash = PasswordHasher.GetHash(null, "", 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_1_6_7_9_11()
+        {
+            var hash = PasswordHasher.GetHash("î", "", 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4_5_7_9_11()
+        {
+            var hash = PasswordHasher.GetHash("î", "salt", 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4_5_7_9_11()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash("î", dp, 0);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_3_4_6_7_9_11()
+        {
+            var hash = PasswordHasher.GetHash("î", "salt", 9310);
+            Assert.NotNull(hash);
+        }
+
+        [Fact]
+        public void Route_0_2_4_6_7_9_11()
+        {
+            string dp = "î";
+            var hash = PasswordHasher.GetHash("î", dp, 9310);
+            Assert.NotNull(hash);
+        }
+
     }
 }
